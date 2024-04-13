de839e405cb8ab241b7a5f5473ece11fb6a03238
Evandro de Almeida Tartari <evandro.a.tartari@gmail.com>
2024-04-13 12:25:52 -0300

    Criação de projetos iniciais

---
+++ src/SignatureReader/Solana.SignatureReader.Batch/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(args);
+ConfigureServices(builder.Services, builder.Configuration);
+
+using IHost host = builder.Build();
+
+host.Run();
+
+static void ConfigureServices(IServiceCollection services, IConfiguration configuration) 
+{
+    #region Hosted Service
+    //services.AddHostedService();
+    #endregion
+}
